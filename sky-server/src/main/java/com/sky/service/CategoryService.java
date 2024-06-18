8d39ef8060786c4e04bbdb2e67fbb7dd135c196e
Rainiwalk <1152830039@qq.com>
2024-06-18 13:38:11 +0800

    业务代码开发-新增分类

--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,9 +1,14 @@
 package com.sky.service;
 
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.result.PageResult;
 
 public interface CategoryService {
 
+    //分类分页查询
     PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    //新增分类
+    void save(CategoryDTO categoryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -2,14 +2,19 @@ package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
 import com.sky.mapper.CategoryMapper;
 import com.sky.result.PageResult;
 import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,6 +23,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    //分类分页查询
     @Override
     public PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO) {
         PageHelper.startPage(categoryPageQueryDTO.getPage(),categoryPageQueryDTO.getPageSize());
@@ -26,5 +32,19 @@ public class CategoryServiceImpl implements CategoryService {
         List<Category> records = page.getResult();
 
         return new PageResult(total,records);
+    }
+
+    //新增分类
+
+    @Override
+    public void save(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO,category);
+        category.setStatus(StatusConstant.ENABLE);
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+        categoryMapper.insert(category);
     }
 }
