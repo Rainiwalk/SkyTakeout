668e2329d92c96c373ad943b24791f53f4094ddf
Rainiwalk <1152830039@qq.com>
2024-07-04 13:16:31 +0800

    业务代码开发-批量删除菜品

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //菜品管理
 @RestController
 @RequestMapping("/admin/dish")
@@ -29,13 +31,22 @@ public class DishController {
         return Result.success();
     }
 
-    //
+    //菜品分页查询
     @GetMapping("/page")
     @ApiOperation("菜品分页查询")
     public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
         log.info("菜品分页查询: {}",dishPageQueryDTO);
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    //批量删除菜品
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result<String> delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除: {}",ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
     }
 
 }
--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +10,11 @@ import java.util.List;
 public interface DishFlavorMapper {
     //批量插入口味数据
     void insertBatch(List<DishFlavor> flavors);
+
+    //根据菜品id删除关联口味信息
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    //根据菜品id集合批量删除关联口味信息
+    void deleteByDishIds(List<Long> dishIds);
 }
