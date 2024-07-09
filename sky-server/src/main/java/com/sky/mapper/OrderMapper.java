bbb812e8a5cb00cdf0701cf9cc8d55c8a3f38e33
Rainiwalk <1152830039@qq.com>
2024-07-09 22:53:48 +0800

    业务代码开发-用户下单

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface OrderMapper {
+
+    /**
+     * 插入订单数据
+     * @param orders
+     */
+    void insert(Orders orders);
+}
